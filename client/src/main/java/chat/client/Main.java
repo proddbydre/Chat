1af9c45949e56d63c700637170929e75065a8988
lai2-24 <lai2-24@itismeucci.com>
2024-11-13 08:09:42 +0100

    base client creata

--- client/src/main/java/chat/client/Main.java
+++ client/src/main/java/chat/client/Main.java
@@ -1,7 +1,27 @@
 package chat.client;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        System.out.println("client partito");
+        Socket s0 = new Socket("localhost", 3000); //socker che dice indirizzo e porta del server a cui connetersi
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s0.getInputStream())); //stream dati in
+        DataOutputStream out = new DataOutputStream(s0.getOutputStream()); //stream dati out
+
+        Scanner input = new Scanner(System.in); //scanner input da tastiera
+
+        
+        
+        input.close();
+        s0.close();
+        System.out.println("client terminato");
     }
 }
