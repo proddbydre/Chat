13aa7e0158a7ec585a1443c860b3b242ec462682
lai2-24 <lai2-24@itismeucci.com>
2024-11-13 10:45:47 +0100

    quasi finiti msg priv lato server

--- server/src/main/java/chat/server/Main.java
+++ server/src/main/java/chat/server/Main.java
@@ -9,9 +9,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Server in ascolto");
         ServerSocket sS0 = new ServerSocket(3000); //Porta dove il server aspetta richiesta
+        DatiCondivisi dC = new DatiCondivisi();
         do {
-            Socket s0 = sS0.accept(); //quando arriva una connessione, viene accettata e rende la nuova porta su cui avverra il vero passaggio di dati   
-            gestioneServizio gS = new gestioneServizio(s0, datiCondivisi);
+            Socket s0 = sS0.accept(); //quando arriva una connessione, viene accettata e rende la nuova porta su cui avverra il vero passaggio di dati
+            System.out.println("Un client si è connesso");   
+            GestioneServizio gS = new GestioneServizio(s0, dC);
             gS.start();
         } while(true);
     }
