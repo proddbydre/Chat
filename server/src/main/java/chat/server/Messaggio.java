5630d88d0d090e34ea84c998809bd6d91e9e295f
Iarossi Leonardo <iarossi.leonardo@itismeucci.com>
2024-11-18 17:37:46 +0100

    creata classe messaggio

---
+++ server/src/main/java/chat/server/Messaggio.java
@@ -0,0 +1,31 @@
+package chat.server;
+
+public class Messaggio {
+    private String mittente;
+    private String destinatario;
+    private String contenuto;
+    private long timestamp;
+
+    public Messaggio(String mittente, String destinatario, String contenuto) {
+        this.mittente = mittente;
+        this.destinatario = destinatario;
+        this.contenuto = contenuto;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public String getMittente() {
+        return mittente;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public String getContenuto() {
+        return contenuto;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
